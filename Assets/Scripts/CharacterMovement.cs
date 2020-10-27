59eb6fa4dfb2abd892f4918796f498d9472669fe
bit1creative <bit1creative@gmail.com>
2020-10-27 09:42:31 +0200

    beta_1
    - added ground with few obstacles
    - added character
    - added character movement script

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float speed;
+    public float walkSpeed = 6.0f;
+    public float sprintSpeed = 12.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20f;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+    }
+
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                speed = sprintSpeed;
+            }
+            else
+            {
+                speed = walkSpeed;
+            }
+
+            moveDirection *= speed;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+                //print("Jump");
+            }
+
+        }
+        else
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), moveDirection.y, Input.GetAxis("Vertical"));
+            speed = walkSpeed;
+            moveDirection.x *= speed;
+            moveDirection.z *= speed;
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
