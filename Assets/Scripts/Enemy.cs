3688622782644e59799b0ffd87c24bbc488bc046
bit1creative <bit1creative@gmail.com>
2020-10-27 12:21:06 +0200

    added enemy beta_v1
    - enemy destroys out char on collisions

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject enemy;
+
+    public void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            // if lives > 1 --> lives--
+            // else {}
+            Destroy(col.gameObject);
+        }
+    }
+}
