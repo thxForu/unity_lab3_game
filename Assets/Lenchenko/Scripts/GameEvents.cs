0717db23328f9c48296771bd27d70f291e9edb8a
thx4u <xavavka12345@gmail.com>
2020-11-26 17:41:54 +0200

    fix

---
+++ Assets/Lenchenko/Scripts/GameEvents.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System;
+using UnityEngine;
+
+public class GameEvents : MonoBehaviour
+{
+    public static GameEvents current;
+    // Start is called before the first frame update
+    private void Awake() 
+    {
+        current = this;    
+    }
+
+    public event Action invulerAb;
+    public void InvulerAb(){
+        if(invulerAb != null){
+            invulerAb();
+        }
+    }
+}
---
+++ Assets/Lenchenko/Scripts/VodkaPower.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VodkaPower : MonoBehaviour
+{
+    public GameObject OffMePls;
+    public float sec;
+    private void Start() {
+        GameEvents.current.invulerAb += InvulerAb;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0,60,0) * Time.deltaTime);
+    }
+
+    private void InvulerAb(){
+        OffMePls.SetActive(false);
+        StartCoroutine(WaitCoroutine());
+
+    }
+
+    private IEnumerator WaitCoroutine(){
+        Enemy.damageAble = false;
+        yield return new WaitForSeconds(sec);
+        Enemy.damageAble = true;
+    }
+}
